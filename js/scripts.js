19ed24d7f5072a12ffaeac693e478bdb546072e7
Trevor <tismetrm@gmail.com>
2019-11-08 15:17:56 -0800

    Add UI logic to final receipt. Connect all callback funcitons to proper vaariables. Display output in receipt.

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,9 @@ function Pizza(size, price, toppings){
   this.size = size;
   this.toppings = toppings;
   this.price = price;
-
 }
 
-Pizza.prototype.calculateToppingsTotal = function(toppingsCount){
+Pizza.prototype.calculateToppingsCosts = function(toppingsCount){
   let toppingsTotal = toppingsCount * 1;
   grandTotal += toppingsTotal;
   return "$" + toppingsTotal.toString() + ".00";
@@ -19,41 +18,33 @@ Pizza.prototype.calculateTaxes = function(total){
   return "$" + Math.round(taxes) + ".00";
 }
 
-Pizza.prototype.addToppings = function(array){
+let pizzaCost = function(pizzaSize){
+  if (pizzaSize === "small"){
+    grandTotal += 8.99;
+    return "$8.99";
+  }
+  if (pizzaSize === "medium"){
+    grandTotal += 11.99;
+    return "$11.99";
+  }
+  if (pizzaSize === "large"){
+    grandTotal += 15.99;
+    return "$15.99";
+  }
+}
+
+let makeToppingsList = function(array){
   for (let i in array){
     if (array[i].name === "topping"){
         toppingsArray.push(array[i].value);
     }
   }
+  return toppingsArray;
 }
 
-Pizza.prototype.pizzaPrice = function(pizzaSize){
-  if (pizzaSize === "small"){
-    grandTotal += 8.99;
-    return 8.99;
-  }
-  if (pizzaSize === "medium"){
-    grandTotal += 11.99;
-    return 11.99;
-  }
-  if (pizzaSize === "large"){
-    grandTotal += 15.99;
-    return 15.99
-  }
-}
-
-// Pizzza.prototype.addToppings = function(allCheckedValues){
-//   for (let i in allCheckedValues){
-//     if (allCheckedValues[i].name === "topping"){
-//         toppingsArray.push(allCheckedValues[i].value);
-//     }
-//   }
-// }
-
-
-let grandTotal = 37;  // Change this back to zero
 let toppingsArray = [];
-let numberOfToppings = toppingsArray.length;
+let toppingsCount = toppingsArray.length;
+let grandTotal = 0;
 
 
 // UI Logic ----------------------
@@ -61,29 +52,26 @@ let numberOfToppings = toppingsArray.length;
 $(document).ready(function(){
 
   $("form#toppings").submit(function(event) {
+
     event.preventDefault();
 
     let pizzaSize = $("#sizes").val();
-    newPizza(pizzaSize);
-
     let allCheckedValues = $("input:checkbox[name=topping]:checked");
-    newPizza.addToppings();
+    let toppings = makeToppingsList(allCheckedValues);
+    let pizzaPrice = pizzaCost(pizzaSize);
+    let toppingsCount = toppingsArray.length;
 
+    let newPizza = new Pizza(pizzaSize, toppings, pizzaPrice);
 
+    $("#pizza-price").text(pizzaPrice);
+    $("#toppings-price").text(newPizza.calculateToppingsCosts(toppingsCount));
+    $("#taxes").text(newPizza.calculateTaxes(grandTotal));
+    $("#grand-total").text(grandTotal);
+    $("#receipt-div").show();
 
+    toppingsArray = [];
+    grandTotal = 0;
 
-    newPizza.addToppings();
-
-    console.log(toppingsArray);
   });
-
-
-
-  $("button#order-now").click(function(){
-    $("#toppings-price").text(newPizza.calculateToppingsTotal(10));  //Update cha
-    $("#taxes").text(newPizza.calculateTaxes(32));  // Update changes
-  })
-
-  let newPizza = new Pizza(pizzaSize, toppingsArray, 12.99)   // Update cahnge
 
 });
