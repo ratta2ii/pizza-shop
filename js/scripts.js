1d3e7beca14be98b5ee680a4b2c87158693852b4
Trevor <tismetrm@gmail.com>
2019-11-08 11:01:25 -0800

    Add document.ready function. Add form submit function for toppings form. Add for loop to loop through checked boxes and push values to toppings array.

--- js/scripts.js
+++ js/scripts.js
@@ -2,8 +2,31 @@
 
 function Pizza(size, price, toppings){
   this.size = size;
-  this.price = price;
   this.toppings = toppings;
+  this.price = price;
+
 }
 
-var pizza1 = new Pizza(.....ADD VARIABLE NAMES OF INPUT VALUES........);
+// var pizza1 = new Pizza(.....ADD VARIABLE NAMES OF INPUT VALUES........);
+
+
+let toppingsArray = [];
+
+$(document).ready(function(){
+
+  $("form#toppings").submit(function(event) {
+    event.preventDefault();
+
+    let allCheckedValues = $("input:checkbox[name=topping]:checked");
+
+    for (let i in allCheckedValues){
+      if (allCheckedValues[i].name === "topping"){
+          toppingsArray.push(allCheckedValues[i].value);
+      }
+    }
+    console.log(toppingsArray);
+  });
+
+
+
+});
