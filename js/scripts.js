450b11d4f77bad1e84efe5d9e0d62960c310e1d3
Trevor <tismetrm@gmail.com>
2019-11-08 10:13:27 -0800

    Add a constructor function to build new pizzas. Research checkboxes and how to collect input  values.

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+// Business Logic
+
+function Pizza(size, price, toppings){
+  this.size = size;
+  this.price = price;
+  this.toppings = toppings;
+}
+
+var pizza1 = new Pizza(.....ADD VARIABLE NAMES OF INPUT VALUES........);
